1b63d26cce364035f785f1ce3870b70980a3dbdb
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2020-05-13 00:04:58 -0300

    - Criando classe database

--- app/controller/database.py
+++ app/controller/database.py
@@ -4,7 +4,7 @@ import sqlalchemy as db
 import pandas as pd
 import datetime
 
-DATABASE_URL = 'mysql+pymysql://admin:gtCovid19*2020@covid-19.csrnyzd4qxmh.sa-east-1.rds.amazonaws.com/covid-19'
+DATABASE_URL = 'mysql+pymysql://admin:gtCovid19*2020@covid-19.csrnyzd4qxmh.sa-east-1.rds.amazonaws.com:3306/covid-19'
 
 class Database():
     engine = db.create_engine(DATABASE_URL, pool_size=200, echo=False, pool_pre_ping=True)
@@ -18,3 +18,35 @@ class Database():
         session = Session(bind=self.connection)        
         session.add(data)
         session.commit()
+
+    def saveIfNew(self, obj):
+        session = Session(bind=self.engine)
+        res = session.query(type(obj)).filter_by(**obj.retData()).first()
+        if res:
+            return res.id
+        session.add(obj)
+        session.flush()
+        session.commit()
+        return session.query(type(obj)).filter_by(**obj.retData()).first().id
+
+    def saveIfNewBulk(self, objList):
+        session = Session(bind=self.engine)
+        res = session.query(type(objList[0])).all()
+        def remKeys(rowDict, oList):
+            for key in rowDict.keys():
+                if key not in oList[0].retData().keys():
+                    del rowDict[key]
+            return rowDict
+        dictList = [remKeys(v.retData(), objList) for v in res]
+        for it in objList:
+            if it.retData() not in dictList:
+                session.add(it)
+        session.commit()
+        return True
+
+    def select(self, obj):
+        session = Session(bind=self.engine)
+        if list(set(obj.retData().values()))[0] == None:
+            return pd.read_sql(session.query(type(obj)).statement, session.bind)
+        else:
+            return pd.read_sql(session.query(type(obj)).filter_by(**obj.retData()).statement, session.bind)
