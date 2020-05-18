25a864e5c2c656f2e19899e828e1d783668abc10
LuanPCunha <luan.pereiracunha@gmail.com>
2020-05-18 01:02:03 -0300

    pequenos ajustes html

--- app/models/models.py
+++ app/models/models.py
@@ -1,24 +1,24 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import MetaData, Table, Column, String, Integer, DateTime, Boolean
-from sqlalchemy.orm import relationship
-from datetime import datetime
 
 Base = declarative_base()
 
 class HealthcareWorker(Base):
     __tablename__ = 'healthcareworker'
+    id = Column('', Integer, primary_key=True)
     name = Column('', String(50))
     created = Column('created_on', DateTime, default=datetime.utcnow)
-    id = Column('', Integer, primary_key=True)
 
     def retData(self):
         return {'name': self.name}
 
 class Paciente(Base):
     __tablename__ = 'paciente'
+    id = Column('', Integer, primary_key=True)
     name = Column('', String(50))
     created = Column('created_on', DateTime, default=datetime.utcnow)
-    id = Column('', Integer, primary_key=True)
 
     def retData(self):
         return {'name': self.name}
