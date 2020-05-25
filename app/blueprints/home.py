803ef94046af3d079db7f0fa98ff720fcf5b1c30
LuanPCunha <luan.pereiracunha@gmail.com>
2020-05-24 22:29:39 -0300

    - Pequenos acertos e Docs

--- app/blueprints/home.py
+++ app/blueprints/home.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, render_template
 from controller.database import Database
-import models.tables as table
+# import models.tables as table
 
 home = Blueprint('Home', __name__)
 
 @home.route('/', methods=['GET'])
 def index():
-    db = Database()
+    # db = Database()
     # user = table.Usuario('Carlos Eduardo', '09697371490', None, '123456')
     # user2 = table.Usuario('Outra Pessoa', '12345678900', None, '123456')
-    user = table.UsuarioPerfil()
+    # user = table.UsuarioPerfil()
     # user2 = table.UsuarioPerfil(2,1)
-    db.saveData(user)
-    db.saveData(user2)
+    # db.saveData(user)
+    # db.saveData(user2)
     return render_template('home.html')
