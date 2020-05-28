9221a1924bb605260f8e60c33320b82780a24734
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2020-05-28 19:32:01 -0300

    - Mudanças Primeira tela

--- app/blueprints/atendimento.py
+++ app/blueprints/atendimento.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request, jsonify, redirect, url_for
-import controller.database as db
+
 atendimento = Blueprint('Atendimento', __name__)
 
 @atendimento.route('/atendimento', methods=['POST'])
 def index():
     if request.method == 'POST':
-        userName = request.form['userName']
-        # db.Database()
+        print(request.form)
+        # userName = request.form['userName']
+        
     return redirect(url_for('Home.index'))
--- app/blueprints/home.py
+++ app/blueprints/home.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
 from controller.database import Database
-# import models.tables as table
 
 home = Blueprint('Home', __name__)
 
@@ -11,11 +10,4 @@ def index():
                     (6,'Doenças Neurológicas'),(7,'Asma'),(8,'Imunodepressão'),
                     (9,'Puérperas'),(10,'Câncer'),(11,'Outros')]
 
-    # db = Database()
-    # user = table.Usuario('Carlos Eduardo', '09697371490', None, '123456')
-    # user2 = table.Usuario('Outra Pessoa', '12345678900', None, '123456')
-    # user = table.UsuarioPerfil()
-    # user2 = table.UsuarioPerfil(2,1)
-    # db.saveData(user)
-    # db.saveData(user2)
     return render_template('primeiroAtendimento.html', comorbidades=comorbidades)
