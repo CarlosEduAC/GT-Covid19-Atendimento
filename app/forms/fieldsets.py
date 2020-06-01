51311c2797153357922d8934c149f78e558010d0
Edson Onildo <inbox.edsononildo@gmail.com>
2020-05-31 21:07:12 -0300

    Adicionando formulários dinámicos

---
+++ app/forms/fieldsets.py
@@ -0,0 +1,17 @@
+import forms.sections as section
+
+fieldsetConjunto1 = {
+    "name": "Conjunto 1",
+    "sections": [
+        section.informacoesBasicas,
+        section.doencasCronicas
+    ]
+}
+
+fieldsetConjunto2 = {
+    "name": "Conjunto 2",
+    "sections": [
+        section.medicamentos,
+        section.esfReferencia
+    ]
+}
