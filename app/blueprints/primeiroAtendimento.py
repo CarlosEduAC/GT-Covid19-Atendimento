c2c18b072b5d864c50e759e6e4398d637f40b7bb
LuanPCunha <luan.pereiracunha@gmail.com>
2020-06-04 00:14:50 -0300

    - Começo da persistencia do primeiro formulario

--- app/blueprints/primeiroAtendimento.py
+++ app/blueprints/primeiroAtendimento.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, render_template
+from flask import request, jsonify, redirect, url_for
 from forms.fieldsets import fieldsetConjunto1, fieldsetConjunto2
+#from controller.primeiroAtendimento import registrar
 primeiroAtendimento = Blueprint('primeiroAtendimento', __name__)
 
 fieldsets = [
     fieldsetConjunto1,
-    fieldsetConjunto2,
+    fieldsetConjunto2
 ]
 
 
-@primeiroAtendimento.route('/primeiroAtendimento', methods=['GET'])
+@primeiroAtendimento.route('/primeiroAtendimento', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        #print(request.form)
+        registrar(request.form)
+
     return render_template('form/form.html', fieldsets=fieldsets)
