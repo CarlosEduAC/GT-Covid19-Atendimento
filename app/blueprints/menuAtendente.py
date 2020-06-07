09217e4595632e6bc2171833e5645fff044c4ea2
matheusantonio <matheus.antonio.c@outlook.com>
2020-06-07 13:12:00 -0300

    Correção na consulta do agendamento e na adição do paciente.

--- app/blueprints/menuAtendente.py
+++ app/blueprints/menuAtendente.py
@@ -1,45 +1,17 @@
 from flask import Blueprint, render_template
 
-from controller.database import Database
-from models.modelsAgendamento import Agendamento, Atendimento
+from controller.agendamentoDao import userAgendamentos
 
 menuAtendente = Blueprint('MenuAtendente', __name__)
 
 @menuAtendente.route('/atendente', methods=['GET'])
 def index():
 
+      
     profissional_atual = 1 #ID do profissional acessando
 
-    db = Database()
+    atendimentos = userAgendamentos(profissional_atual)
 
-    agendamentos = Agendamento.query.filter_by(idProfissional = profissional_atual).all()
-
-    atendimentos = []
-
-    for agendamento in agendamentos:
-        atendimento = Atendimento.query.filter_by(id = agendamento.idAtendimento).first()
-        atendimentos.append(
-            {'diaAgendamento':agendamento.dia,
-             'primeiro' : atendimento.primeiro,
-             'diaAtendimento' : atendimento.dia}
-        )
-
-    '''
-
-    atendimentos = [
-        {'diaAgendamento':'10/5/2020',
-         'primeiro' : True,
-         'diaAtendimento' : '10/5/2020'},
-        {'diaAgendamento':'10/5/2020',
-         'primeiro' : False,
-         'diaAtendimento' : '10/5/2020'},
-        {'diaAgendamento':'10/5/2020',
-         'primeiro' : False,
-         'diaAtendimento' : '10/5/2020'},
-        {'diaAgendamento':'10/5/2020',
-         'primeiro' : True,
-         'diaAtendimento' : '10/5/2020'},
-    ]
-    '''
+    print(atendimentos)
 
     return render_template('menuAtendente.html', atendimentos = atendimentos)
--- app/blueprints/registrarPaciente.py
+++ app/blueprints/registrarPaciente.py
@@ -1,25 +1,23 @@
 from flask import Blueprint, render_template, redirect, request, url_for
 from datetime import datetime
 from controller.database import Database
-from models.models import Paciente
+from controller.util import savePaciente
 
 registrarPaciente = Blueprint('Paciente', __name__)
 
 @registrarPaciente.route('/paciente', methods=['GET', 'POST'])
 def registrar():
     if request.method == 'POST':
-        nome = request.form['nome']
+        """ nome = request.form['nome']
         cpf = request.form['cpf']
         ocupacao = request.form['ocupacao']
+        """
         sexo = request.form['sexo']
         raca = request.form['raca']
         dataNasc = request.form['data_nasc']
+        
+        savePaciente(None, None, sexo, raca, datetime.strptime(dataNasc, '%d/%m/%Y'))
 
-        db=Database()
-        
-        paciente = Paciente(nome, cpf, ocupacao, sexo, raca, datetime.strptime(dataNasc, '%d/%m/%Y'))
-        
-        db.saveData(paciente)
     
     return render_template('paciente.html')
 
