42e91925f223359831781a016daca34417046b30
Nayara Cunha <nayaracn@outlook.com>
2020-06-18 19:00:36 -0300

    Feito: Login - Logout - Atenticacao em menuAtendimento

--- app/blueprints/login.py
+++ app/blueprints/login.py
@@ -1,13 +1,31 @@
 from flask import Blueprint, render_template, redirect, request, url_for
+from flask_login import login_user, logout_user
 from models.models import AdmSaude
 from controller.database import Database
 
 login = Blueprint('Login', __name__)
 
-@login.route('/login', methods=['GET'])
-def index():
+@login.route('/login', methods=['GET', 'POST'])
+def loginMetodo():
+
+    if request.method == 'POST':
+        cpf = request.form['cpf']
+        sh = request.form['senha']
+
+        db = Database()
+        
+        userCPF = db.selectData(AdmSaude,cpf=cpf) #AdmSaude(7,"maria", 12345678910, 675, 1, sh)
+
+        if not userCPF or not userCPF.verificaSenha(sh):
+            return redirect(url_for('login'))
+        else:
+            login_user(userCPF) 
+            return redirect(url_for('MenuAtendente.index'))
+    
+
     return render_template('login.html')
     
-@login.route('/logout', methods=['GET'])
+@login.route('/logout')
 def logout():
+    logout_user()
     return render_template('login.html')
--- app/blueprints/menuAtendente.py
+++ app/blueprints/menuAtendente.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, LoginManager
 
 from dao.agendamento import userAgendamentos
 
 menuAtendente = Blueprint('MenuAtendente', __name__)
 
 @menuAtendente.route('/', methods=['GET'])
+@login_required #só permite acessar esta pagina sé estiver logado
+
 def index():
     profissional_atual = 1 #ID do profissional acessando
 
