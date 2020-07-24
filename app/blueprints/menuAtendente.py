c3cbf32ab25334c72c62ee1853e0635a0aaff0ad
bazilio <carlosbazilio@gmail.com>
2020-07-24 00:22:52 -0300

    Geração de PDF de Agendamentos

--- app/blueprints/menuAtendente.py
+++ app/blueprints/menuAtendente.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 from flask_login import login_required, LoginManager, current_user
 from datetime import datetime
+from flask_weasyprint import HTML, render_pdf
 
 from dao.agendamento import userAgendamentos
 
@@ -9,12 +10,23 @@ menuAtendente = Blueprint('MenuAtendente', __name__)
 @menuAtendente.route('/', methods=['GET'])
 @login_required 
 def index():
-
     atendimentos = list(map(
         setImportance, 
         userAgendamentos(current_user.id)))
 
-    return render_template('menuAtendente.html', atendimentos = atendimentos, formatTime = datetime.strftime)
+    return render_template('menuAtendente.html', atendimentos = atendimentos, formatTime = datetime.strftime, pdf = False)
+
+@menuAtendente.route('/pdf', methods=['GET'])
+@login_required 
+def indexPDF():
+    atendimentos = list(map(
+        setImportance, 
+        userAgendamentos(current_user.id)))
+
+    template_renderizado = render_template('agendamento.html', atendimentos = atendimentos, formatTime = datetime.strftime, pdf = True)
+
+    return render_pdf(HTML(string=template_renderizado))
+
 
 def setImportance(atendimento):
     today = datetime.today().date()
