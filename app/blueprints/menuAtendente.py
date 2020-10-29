7bdbdf75b32957452420332c96180c139af2cbc6
matheusantonio <matheus.antonio.c@outlook.com>
2020-10-29 16:20:04 -0300

    Rotas necessarias para o service worker

--- app/blueprints/menuAtendente.py
+++ app/blueprints/menuAtendente.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, jsonify
 from flask_login import login_required, LoginManager, current_user
 from datetime import datetime
 from controller.pdfInclusao import incluiPdf
 from blueprints.login import ler_dados
 
 from dao.agendamento import userAgendamentos
+from dao.paciente import getPaciente
 
 menuAtendente = Blueprint('MenuAtendente', __name__)
 
@@ -17,6 +18,16 @@ def sw():
 @login_required 
 def index():
     return render_template('menuAtendente.html', atendimentos = getAtendimentos(), formatTime = datetime.strftime, dados = ler_dados(), pdf = False, incluiBotaoPdf = incluiPdf())
+
+@menuAtendente.route('/dados', methods=['GET'])
+@login_required
+def dados():
+    agendamentos = getAtendimentos()
+    return jsonify({
+        "agendamentos" : agendamentos,
+        "pacientes" : [getPaciente(ag['idPaciente']).to_dict() for ag in agendamentos]
+    
+    }), 200
 
 def setImportance(atendimento):
     today = datetime.today().date()
