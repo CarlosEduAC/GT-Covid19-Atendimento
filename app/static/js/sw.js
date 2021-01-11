ed94c6a593ebdc7473554748ba1ef465811cd521
Guilherme Nunes <guilherme.martins@plugg.to>
2021-01-11 20:08:45 -0300

    melhorias na implementação offline-first

--- app/static/js/sw.js
+++ app/static/js/sw.js
@@ -2,7 +2,7 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v2.0';
-const RUNTIME = 'runtime';
+const RUNTIME = 'runtime-v2.0';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
